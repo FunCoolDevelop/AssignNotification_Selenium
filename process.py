f988292bb018821b08888df5bbdb368fa3a1291b
민상연 <judemin2087@naver.com>
2020-12-21 21:07:25 +0900

    Crawling Function
    Crawl HelloLMS with userid and pw

--- process.py
+++ process.py
@@ -4,24 +4,69 @@ from time import sleep
 import accounts
 
 options = webdriver.ChromeOptions()
-options.add_argument('window-size=1920,1080')
+options.add_argument('window-size=1280,720')
 
 driver = webdriver.Chrome('chromedriver', options=options)
 driver.implicitly_wait(10)
 
 driver.get(url='http://ecampus.konkuk.ac.kr/ilos/main/member/login_form.acl')
 
-search_box = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+id_input = driver.find_element_by_id('usr_id')
+id_input.send_keys(accounts.id)
 
-search_box.send_keys('greeksharifa.github.io')
-search_box.send_keys(Keys.RETURN)
+pw_input = driver.find_element_by_id('usr_pwd')
+pw_input.send_keys(accounts.pw)
 
-elements = driver.find_elements_by_xpath('//*[@id="rso"]/div[*]/div/div[1]/a/h3/span')
+login_btn = driver.find_element_by_id('login_btn')
+login_btn.click()
 
-for element in elements:
-    print(element.text)
-    print(element.text, file=open('gorio.txt', 'w', encoding='utf-8'))
+lessons = driver.find_elements_by_class_name('sub_open')
+sub_len = len(lessons)
+assignList = []
+for i in range(0,sub_len) :
+    tmpList = [] ## tmpList[0] is name of assign
+    
+    lessons = driver.find_elements_by_class_name('sub_open')
+    lns = lessons[i]
+    print(lns.text)
+    tmpList.append(lns.text)
+    lns.click()
+    homw_tab = driver.find_element_by_id('menu_report')
+    homw_tab.click()
 
+    names = driver.find_elements_by_class_name("subjt_top")
+    dates = driver.find_elements_by_class_name("number")
+    
+    if names == None :
+        l = 0
+    else :
+        l = len(names)
+
+    for j in range(0,l):
+        nn = names[j].text
+        nd = dates[((j+1)*5) - 1].text
+        tmp = []
+        tmp.append(nn)
+        tmp.append(nd)
+        tmpList.append(tmp)
+        ##print(nn)
+        ##print(nd)
+    assignList.append(tmpList)
+
+    driver.back()
+    driver.back()
+
+print('Crawling Finished')
+print(assignList)
+
+result = ''
+for i in assignList :
+    ilen = len(i)
+    result += i[0] + '\n'
+    for j in range(1,ilen) :
+        result += i[j][0] + ' ' + i[j][1] + '\n'
+    result += '\n'
+print(result, file=open('Assigns.txt', 'w', encoding='utf-8'))
 
 sleep(3)
 driver.close()
