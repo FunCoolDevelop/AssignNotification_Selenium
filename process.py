1ac5f49013adce4ad9206afcc082f7abe429901e
민상연 <judemin2087@naver.com>
2020-12-21 18:01:18 +0900

    TestCode
    Crawling test

---
+++ process.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import accounts
+
+options = webdriver.ChromeOptions()
+options.add_argument('window-size=1920,1080')
+
+driver = webdriver.Chrome('chromedriver', options=options)
+driver.implicitly_wait(10)
+
+driver.get(url='http://ecampus.konkuk.ac.kr/ilos/main/member/login_form.acl')
+
+search_box = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+
+search_box.send_keys('greeksharifa.github.io')
+search_box.send_keys(Keys.RETURN)
+
+elements = driver.find_elements_by_xpath('//*[@id="rso"]/div[*]/div/div[1]/a/h3/span')
+
+for element in elements:
+    print(element.text)
+    print(element.text, file=open('gorio.txt', 'w', encoding='utf-8'))
+
+sleep(3)
+driver.close()
