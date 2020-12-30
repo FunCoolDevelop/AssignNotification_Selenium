db340d4aa314d6fffab0fceabed72abeef0a7955
민상연 <judemin2087@naver.com>
2020-12-30 16:04:52 +0900

    feat : Url mapping
    mapped url, index page

--- django_server/crawlEngine/crawler/urls.py
+++ django_server/crawlEngine/crawler/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from crawler import views
 
 urlpatterns = [
-
+    path('',views.index, name = 'index'),
+    path('crawlAll',views.crawlAll, name = 'crawlAll'),
+    path('<int:stid>',views.crawlSingle, name = 'crawlSingle'),
 ]
--- django_server/crawlEngine/crawler/views.py
+++ django_server/crawlEngine/crawler/views.py
@@ -1,2 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Assign, College, Student, Course, Quiz, TeamPro
 
+def index(request) :
+    students = Student.objects.all()
+    return render(request, 'index.html',{"students":students})
+
+def crawlSingle(request,stid) :
+    sid = int(stid)
+    return render(request, 'crawlPage.html')
+
+def crawlAll(request) :
+    return render(request, 'crawlPage.html')
