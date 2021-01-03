782f8f3fa221466a1df50a93826e7db8ae1bccba
민상연 <judemin2087@naver.com>
2021-01-03 15:38:42 +0900

    feat : Temporal url crawling
    added crawlTemp

--- django_server/crawlEngine/crawler/views.py
+++ django_server/crawlEngine/crawler/views.py
@@ -30,8 +30,11 @@ def operation(sid,stObj) :
         crawl(stObj)
 
 def crawl(student):
-    print("Crawling account name [" + student.name + "]")
+    crawlTemp(student) # 학기중엔 crawlTemp 사용x
 
+    '''
+    print("Crawling account name [" + student.name + "]")
+    # login
     college = College.objects.filter(Q(id=student.college_id)).get()
     options = webdriver.ChromeOptions()
     options.add_argument('window-size=1280,720')
@@ -49,9 +52,11 @@ def crawl(student):
 
     login_btn = driver.find_element_by_id('login_btn')
     login_btn.click()
-
+    #
+    # crawl course(lesson) in main page
     lessons = driver.find_elements_by_class_name('sub_open')
     sub_len = len(lessons)
+    # crawl assign
     assignList = []
     for i in range(0,sub_len) :
         tmpList = []
@@ -73,33 +78,114 @@ def crawl(student):
             l = len(names)
 
         for j in range(0,l):
-            nn = names[j].text
-            nd = dates[((j+1)*5) - 1].text
-            tmp = []
-            tmp.append(nn)
-            tmp.append(nd)
-            tmpList.append(tmp)
-            #print(nn)
-            #print(nd)
-        assignList.append(tmpList)
+                nn = names[j].text # name
+                ns = dates[((j+1)*5) - 3].text + '/' + dates[((j+1)*5) - 2].text # score
+                nd = dates[((j+1)*5) - 1].text # date
+                tmp = []
+                tmp.append(nn)
+                tmp.append(ns)
+                tmp.append(nd)
+                tmpList.append(tmp)
+            assignList.append(tmpList)
 
         driver.back()
         driver.back()
 
     print('Crawling Finished')
 
-    result = '' 
+    result = ''
     for i in assignList : # string transformation
         ilen = len(i)
         result += i[0] + '\n'
         for j in range(1,ilen) :
-            result += i[j][0] + ' ' + i[j][1] + '\n'
+            result += i[j][0] + '  |  ' + i[j][1] + '  |  ' + i[j][2] + '\n'
         result += '\n'
-    #print(result)
+    print(result)
+
+    driver.close()
+    postProcess(assignList)
+    '''
+
+def crawlTemp(student): # 학기중이 아니므로 다른 경로로 크롤링
+    print("Crawling account name [" + student.name + "]")
+    # login
+    college = College.objects.filter(Q(id=student.college_id)).get()
+    options = webdriver.ChromeOptions()
+    options.add_argument('window-size=1280,720')
+
+    driver = webdriver.Chrome(r"C:/chromedriver.exe", options=options)
+    driver.implicitly_wait(1)
+
+    driver.get(url=college.home_url)
+
+    id_input = driver.find_element_by_id('usr_id')
+    id_input.send_keys(student.login_id)
+
+    pw_input = driver.find_element_by_id('usr_pwd')
+    pw_input.send_keys(student.login_pw)
+
+    login_btn = driver.find_element_by_id('login_btn')
+    login_btn.click()
+    #
+    # 수강과목 페이지 접속
+    courseBtn = driver.find_elements_by_class_name('icon-nm')
+    courseBtn[0].click()
+    #
+    lessons = driver.find_elements_by_class_name('content-title')
+    sub_len = len(lessons)
+    # crawl assign
+    assignList = []
+    for i in range(0,sub_len) :
+        tmpList = []
+        
+        lessons = driver.find_elements_by_class_name('content-title')
+        lns = lessons[i]
+
+        try :
+            print("Course " + (str)(i + 1) + "    " + lns.text)
+            tmpList.append(lns.text)
+            lns.click()
+            homw_tab = driver.find_element_by_id('menu_report')
+            homw_tab.click()
+
+            names = driver.find_elements_by_class_name("subjt_top")
+            dates = driver.find_elements_by_class_name("number")
+            
+            if names == None :
+                l = 0
+            else :
+                l = len(names)
+
+            for j in range(0,l):
+                nn = names[j].text # name
+                ns = dates[((j+1)*5) - 3].text + '/' + dates[((j+1)*5) - 2].text # score
+                nd = dates[((j+1)*5) - 1].text # date
+                tmp = []
+                tmp.append(nn)
+                tmp.append(ns)
+                tmp.append(nd)
+                tmpList.append(tmp)
+            assignList.append(tmpList)
+
+            driver.back()
+            driver.back()
+        except :
+            break
+
+    print('Crawling Finished')
+
+    result = ''
+    for i in assignList : # string transformation
+        ilen = len(i)
+        result += i[0] + '\n'
+        for j in range(1,ilen) :
+            result += i[j][0] + '  |  ' + i[j][1] + '  |  ' + i[j][2] + '\n'
+        result += '\n'
+    print(result)
 
     driver.close()
     postProcess(assignList)
 
 def postProcess(assignRes):
     # assignRes의 각 행 0번째 값은 과목의 이름
-    print(assignRes)
+    print('!')
